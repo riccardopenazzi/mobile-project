f07eb46740f6fb22ea98a6ea1c68d58828a67a90
penazziriccardo <penazziriccardo17@gmail.com>
2024-04-21 11:04:40 +0200

    [ADD] added login screen only ui part, no color customization

--- app/src/main/java/it/unibo/noteforall/ui/screen/LoginScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/LoginScreen.kt
@@ -1,5 +1,6 @@
 package it.unibo.noteforall.ui.screen
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,25 +9,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 
 @Composable
-fun SignupScreen() {
+fun LoginScreen() {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -42,36 +45,32 @@ fun SignupScreen() {
                     text = "Note For All",
                     fontWeight = FontWeight.Bold,
                     fontSize = 30.sp,
-                    modifier = Modifier.padding(vertical = 30.dp)
-                )
-                Icon(
-                    imageVector = Icons.Outlined.AccountCircle,
-                    contentDescription = "Select profile image",
-                    modifier = Modifier
-                        .size(50.dp)
-                        .clickable { /* TODO */ }
+                    modifier = Modifier.padding(vertical = 50.dp)
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 listOf(
-                    "Name",
-                    "Surname",
-                    "Email",
-                    "Username",
-                    "Password",
-                    "Repeat password"
+                    "Email or username",
+                    "Password"
                 ).forEach { label ->
                     Spacer(modifier = Modifier.height(8.dp))
                     OutlinedTextField(value = "", onValueChange = {}, label = {
                         Text(text = label)
                     })
                 }
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(14.dp))
                 Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Signup")
+                    Text(text = "Login")
                 }
                 Spacer(modifier = Modifier.height(8.dp))
-                TextButton(onClick = { /*TODO*/ }) {
-                    Text(text = "Already have an account?")
+                TextButton(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .border(2.dp, Color.Black)
+                        .clip(
+                            RoundedCornerShape(6.dp)
+                        )
+                        .padding(4.dp)
+                ) {
+                    Text(text = "Don't have an account?")
                 }
             }
         }
