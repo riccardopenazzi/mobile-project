94c48b5eb634f6bb3f5b0cd3ef5dc0eab849b3ab
penazziriccardo <penazziriccardo17@gmail.com>
2024-04-22 13:24:32 +0200

    [MOD] modified main activity, now supports top and bottom bar by default, no need to add them in other screens

--- app/src/main/java/it/unibo/noteforall/MainActivity.kt
+++ app/src/main/java/it/unibo/noteforall/MainActivity.kt
@@ -1,10 +1,8 @@
 package it.unibo.noteforall
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -12,26 +10,23 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import it.unibo.noteforall.ui.composables.AppBar
 import it.unibo.noteforall.ui.composables.BottomBar
+import it.unibo.noteforall.ui.screen.home.HomeScreen
 import it.unibo.noteforall.ui.theme.NoteForAllTheme
 import it.unibo.noteforall.utils.bottomNavigationItems
 
 class MainActivity : ComponentActivity() {
-
     val items = listOf("Home", "Saved", "Search", "Profile")
     val icons = listOf(
         Icons.Outlined.Home,
@@ -39,8 +34,8 @@ class MainActivity : ComponentActivity() {
         Icons.Outlined.Search,
         Icons.Outlined.Person
     )
+    val db = Firebase.firestore
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -65,27 +60,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     ) { contentPadding ->
-                        println(contentPadding)
-                        Spacer(modifier = Modifier.padding(200.dp))
-                        Greeting("Android")
+                        HomeScreen(modifier = Modifier.padding(contentPadding))
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!"
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteForAllTheme {
-        Greeting("Android")
     }
 }
