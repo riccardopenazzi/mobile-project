0d44f3202a6dd7db901e2ac5ad6cb3447be91faa
penazziriccardo <penazziriccardo17@gmail.com>
2024-04-22 11:33:54 +0200

    [ADD] added first version of my profile screen, it uses a lot of placeholders and includes a top bar just to test

--- app/src/main/java/it/unibo/noteforall/ui/screen/myProfile/MyProfileScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/myProfile/MyProfileScreen.kt
@@ -1,8 +1,110 @@
 package it.unibo.noteforall.ui.screen.myProfile
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Icon
+import it.unibo.noteforall.ui.composables.AppBar
+import it.unibo.noteforall.ui.composables.NoteCard
+import it.unibo.noteforall.ui.theme.Teal800
 
 @Composable
 fun MyProfileScreen() {
+    Scaffold(
+        topBar = { AppBar(title = "My Profile") }
+    ) { contentPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize()
+        ) {
+            item {
+                Column(
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                        .fillMaxSize()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle,
+                            contentDescription = "Profile pic",
+                            modifier = Modifier.size(80.dp),
+                            tint = Color.Black
+                        )
+                        Spacer(Modifier.width(10.dp))
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Name",
+                                modifier = Modifier
+                                    .border(1.dp, Teal800, RoundedCornerShape(30))
+                                    .padding(6.dp)
+                                    .fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Surname",
+                                modifier = Modifier
+                                    .border(1.dp, Teal800, RoundedCornerShape(30))
+                                    .padding(6.dp)
+                                    .fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(
+                        text = "Badges here",
+                        modifier = Modifier
+                            .border(1.dp, Teal800, RoundedCornerShape(30))
+                            .padding(30.dp)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                    PrintUserNotes()
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun PrintUserNotes() {
+    for (i in 0..9) {
+        NoteCard()
+    }
 }
