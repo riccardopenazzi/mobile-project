03b480f3e4b1b1e7e76d21ea377f52a8b7dd0448
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-04-24 16:09:07 +0200

    [ADD] Added LocationService, first implementation inside SignupScreen (not working well)

--- app/src/main/java/it/unibo/noteforall/ui/screen/signup/SignupScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/signup/SignupScreen.kt
@@ -1,38 +1,44 @@
 package it.unibo.noteforall.ui.screen.signup
 
+import android.Manifest
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.AddLocationAlt
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.firestore.FirebaseFirestore
 import it.unibo.noteforall.utils.CurrentUser
 import it.unibo.noteforall.utils.CurrentUserSingleton
+import it.unibo.noteforall.utils.LocationService
+import it.unibo.noteforall.utils.PermissionStatus
+import it.unibo.noteforall.utils.rememberPermission
 
 @Composable
 fun SignupScreen(db: FirebaseFirestore) {
@@ -54,6 +60,42 @@ fun SignupScreen(db: FirebaseFirestore) {
     var repeatPassword by remember {
         mutableStateOf("")
     }
+
+    val ctx = LocalContext.current
+
+    val locationService = LocationService(ctx)
+
+    val locationPermission = rememberPermission(
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) { status ->
+        when (status) {
+            PermissionStatus.Granted ->
+                locationService.requestCurrentLocation()
+
+            PermissionStatus.Denied -> {}
+                // Gestire il caso di negazione dei permessi
+                // actions.setShowLocationPermissionDeniedAlert(true)
+
+            PermissionStatus.PermanentlyDenied -> {}
+                // Gestire il caso di negazione dei permessi
+                // actions.setShowLocationPermissionPermanentlyDeniedSnackbar(true)
+
+            PermissionStatus.Unknown -> {}
+        }
+    }
+
+    fun requestLocation() {
+        if (locationPermission.status.isGranted) {
+            locationService.requestCurrentLocation()
+        } else {
+            locationPermission.launchPermissionRequest()
+        }
+    }
+
+    LaunchedEffect(locationService.isLocationEnabled) {
+        //actions.setShowLocationDisabledAlert(locationService.isLocationEnabled == false)
+    }
+
     LazyColumn(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -103,6 +145,10 @@ fun SignupScreen(db: FirebaseFirestore) {
                 value = repeatPassword,
                 onValueChange = { repeatPassword = it },
                 label = { Text(text = "Repeat password") })
+            Spacer(modifier = Modifier.height(8.dp))
+            IconButton(onClick = ::requestLocation) {
+                Icon(Icons.Outlined.AddLocationAlt, "Add location icon")
+            }
             Spacer(modifier = Modifier.height(8.dp))
             Button(onClick = {
                 execSignup(
