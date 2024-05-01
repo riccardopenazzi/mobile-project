d227c7899ec0d8fbb4c98ba6b327c480a5bae31f
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-05-01 12:38:36 +0200

    [MOD] Implemented the star icon clickable in note card

--- app/src/main/java/it/unibo/noteforall/ui/composables/NoteCard.kt
+++ app/src/main/java/it/unibo/noteforall/ui/composables/NoteCard.kt
@@ -1,27 +1,20 @@
 package it.unibo.noteforall.ui.composables
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Download
-import androidx.compose.material.icons.outlined.Image
-import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Image
-import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,11 +23,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import it.unibo.noteforall.ui.theme.Teal800
@@ -56,10 +49,25 @@ fun NoteCard(isExtended: Boolean = false, navController: NavHostController) {
             Row (
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "", modifier = Modifier.size(40.dp))
+                Icon(
+                    imageVector = Icons.Rounded.AccountCircle,
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
                 Spacer(modifier = Modifier.width(10.dp))
                 Text(text = "Author", modifier = Modifier.weight(1f))
-                Icon(imageVector = Icons.Outlined.StarBorder, contentDescription = "")
+                val starIcon = remember { mutableStateOf(Icons.Outlined.StarBorder) }
+                IconButton(onClick = {
+                    when (starIcon.value) {
+                        Icons.Outlined.StarBorder -> starIcon.value = Icons.Filled.Star
+                        else -> starIcon.value = Icons.Outlined.StarBorder
+                    }
+                }) {
+                    Icon(
+                        imageVector = starIcon.value,
+                        contentDescription = ""
+                    )
+                }
             }
             Spacer(modifier = Modifier.size(10.dp))
             Icon(imageVector = Icons.Rounded.Image, contentDescription = "Note preview", modifier = Modifier.size(100.dp))
