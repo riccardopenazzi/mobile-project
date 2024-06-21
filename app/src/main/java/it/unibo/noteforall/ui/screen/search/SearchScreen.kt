9646a692b9f780d2bf25263179ce5db51a058569
penazziriccardo <penazziriccardo17@gmail.com>
2024-06-21 17:32:52 +0200

    [FIX] fixed search screen

--- app/src/main/java/it/unibo/noteforall/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/search/SearchScreen.kt
@@ -2,16 +2,19 @@ package it.unibo.noteforall.ui.screen.search
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,8 +31,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.firebase.firestore.FirebaseFirestore
 import it.unibo.noteforall.data.firebase.StorageUtil
@@ -44,7 +50,11 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun SearchScreen(db: FirebaseFirestore, navController: NavHostController, posts: MutableList<Note>) {
+fun SearchScreen(
+    db: FirebaseFirestore,
+    navController: NavHostController,
+    posts: MutableList<Note>
+) {
     var text by remember { mutableStateOf("") }
     val focusManager = LocalFocusManager.current
     var isLaunched by remember { mutableStateOf(false) }
@@ -92,6 +102,7 @@ fun SearchScreen(db: FirebaseFirestore, navController: NavHostController, posts:
                         isListUpdated = true
                         CoroutineScope(Dispatchers.Main).launch {
                             searchPost(posts, db, text)
+                            isSearched = false;
                         }
                         focusManager.clearFocus()
                     }),
@@ -101,11 +112,27 @@ fun SearchScreen(db: FirebaseFirestore, navController: NavHostController, posts:
             if (isSearched && posts.size == 0) {
                 item { LoadingAnimation() }
             } else {
-                if (isListUpdated) {
-                    items(posts) { post ->
-                        if (isSearched) {
-                            isSearched = false
+                if (isListUpdated && posts.size == 0) {
+                    item {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 20.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(Icons.Outlined.Clear, "No posts to show icon")
+                            Spacer(Modifier.width(10.dp))
+                            Text(
+                                text = "No posts to show",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            )
                         }
+                    }
+                } else {
+                    items(posts) { post ->
                         NoteCard(navController = navController, note = post, db = db)
                     }
                 }
