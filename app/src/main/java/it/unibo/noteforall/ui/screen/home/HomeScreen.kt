e3598744243c8d25ba9f63107ae5ac382429a7cd
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-06-21 17:06:14 +0200

    [FIX] Fixed no posts to show label

--- app/src/main/java/it/unibo/noteforall/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/home/HomeScreen.kt
@@ -1,12 +1,18 @@
 package it.unibo.noteforall.ui.screen.home
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
@@ -20,7 +26,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.firebase.firestore.FirebaseFirestore
 import it.unibo.noteforall.data.firebase.StorageUtil.Companion.loadHomePosts
@@ -69,7 +80,22 @@ fun HomeScreen(navController: NavHostController, db: FirebaseFirestore, posts: M
                 item { LoadingAnimation() }
             } else {
                 if (posts.size == 0) {
-                    item { Text(text = "No posts to show, remove filter") }
+                    item {
+                        Row(
+                            modifier = Modifier.fillMaxSize().padding(top = 20.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(Icons.Outlined.Clear, "No posts to show icon")
+                            Spacer(Modifier.width(10.dp))
+                            Text(
+                                text = "No posts to show, remove filter",
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
                 } else {
                     items(posts) { post ->
                         NoteCard(navController = navController, note = post, db = db)
--- app/src/main/java/it/unibo/noteforall/ui/screen/saved/SavedNotesScreen.kt
+++ app/src/main/java/it/unibo/noteforall/ui/screen/saved/SavedNotesScreen.kt
@@ -1,11 +1,21 @@
 package it.unibo.noteforall.ui.screen.saved
 
-import android.util.Log
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.firebase.firestore.FirebaseFirestore
 import it.unibo.noteforall.data.firebase.StorageUtil
@@ -39,7 +52,7 @@ fun SavedNotesScreen(navController: NavHostController, db: FirebaseFirestore) {
         }
     }
 
-    Scaffold() { paddingValues ->
+    Scaffold { paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .padding(paddingValues)
@@ -48,7 +61,21 @@ fun SavedNotesScreen(navController: NavHostController, db: FirebaseFirestore) {
             horizontalAlignment = Alignment.Start
         ) {
             if (isEmpty.get()) {
-                item { Text(text = "No post saved yet", textAlign = TextAlign.Center) }
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Icon(Icons.Outlined.Clear, "No posts saved icon")
+                        Spacer(Modifier.width(10.dp))
+                        Text(
+                            text = "No posts saved yet",
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                    }
+                }
             } else {
                 items(posts) { post ->
                     NoteCard(navController = navController, note = post, db = db)
